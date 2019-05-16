799860c1ab04b2a1ba754f1e67007aab6d25bae6
Bohdan Kolomiiets <bohdank@rabota.ua>
2019-05-16 11:59:25 +0300

    save cv to vacancy flow finished

--- save-cv.js
+++ save-cv.js
@@ -1,21 +1,27 @@
 let recruserSaveBlock = document.getElementById('saveCv-step');
 let recruserSaveBtn = document.getElementById('recruser-save-btn');
-let recruserUserInDbText = document.getElementById('recruser-user-in-db');
+let recruserMultipleProfilesFoundText = document.getElementById('recruser-multiple-users-in-db');
+let recruserFoundUserInDbText = document.getElementById('recruser-user-in-db');
 let recruserAddCvToAnotherVacancyBtn = document.getElementById('recruser-select-another-vacancy-btn');
 
 let recruserSelectVacancyBlock = document.getElementById('recruser-selectVacancy-step');
 let recruserSelectVacancyAutocomplete = document.getElementById('recruser-selectVacancy-autocomplete');
 let recruserSelectVacancyNextStep = document.getElementById('recruser-selectVacancy-next-step');
+let selectVacancyValidationEl = document.getElementById('recruser-selectVacancy-validation');
 
 let recruserSelectStepBlock = document.getElementById('recruser-selectStep-step');
 let recruserSelectStepAutocomplete = document.getElementById('recruser-selectStep-autocomplete');
 let recruserSelectStepNextStep = document.getElementById('recruser-selectStep-next-step');
+let selectStepValidationEl = document.getElementById('recruser-selectStep-validation');
 
 let recruserDoneBlock = document.getElementById('recruser-done-step');
+
+let recruserProfileLink = document.getElementById('recruser-profile-link');
 
 initSteps();
 
 (async () => {
+    await setUserIfTestingEnvironment();
     await setStep(window.RecruserNoneStep);
 })();
 
@@ -44,15 +50,15 @@ async function ManageMarkup(step) {
             break;
         }
         case window.RecruserParseCvIfNotExistInDbStep: {
-            setupParseCvStep();
+            await setupParseCvStep();
             break;
         }
         case window.RecruserSelectVacancyStep: {
-            SetupSelectVacancyStep();
+            await SetupSelectVacancyStep();
             break;
         }
         case window.RecruserSelectStepStep: {
-            setupSelectStepStep();
+            await setupSelectStepStep();
             break;
         }
         case window.RecruserDoneStep: {
@@ -62,36 +68,47 @@ async function ManageMarkup(step) {
     }
 }
 
-function setupParseCvStep() {
-    var foundCvId = null;//'foundCvId';//TODO check in DB using API
+async function setupParseCvStep() {
+    let possibleCvIds = await getPossibleCvIdsByFullNameOrSoruceUrlInDb();
     recruserSaveBlock.style.display = 'block';
-    if (foundCvId) {
-        recruserUserInDbText.style.display = 'block';
-        recruserAddCvToAnotherVacancyBtn.onclick = (e) => {
-            e.preventDefault();
-            window.cvId = foundCvId;
-            // parseCv().then(cv => {
-            //     window.cv = cv;
-            // });
-            setStep(window.RecruserSelectVacancyStep);
-        };
-    } else {
+
+    if (possibleCvIds.length == 0) {
         recruserSaveBtn.style.display = 'block';
         recruserSaveBtn.onclick = (e) => {
             e.preventDefault();
-            Promise.resolve(() => {
-                //parse CV
-                window.cvId = 'savedCvId';
-
-            });
+            parseCvAndSetCvId();
             setStep(window.RecruserSelectVacancyStep);
         };
+    } else if (possibleCvIds.length == 1) {
+        recruserFoundUserInDbText.style.display = 'block';
+        recruserAddCvToAnotherVacancyBtn.onclick = (e) => {
+            e.preventDefault();
+            window.recruserCvId = possibleCvIds[0];
+            setStep(window.RecruserSelectVacancyStep);
+        };
+        recruserProfileLink.setAttribute('href', `https://account.recruser.com/cv-list/${window.recruserCvId}`);
+        recruserProfileLink.setAttribute('target', '_blank');
+    } else {
+        recruserMultipleProfilesFoundText.style.display = 'block';
+        recruserProfileLink.setAttribute('href', `https://account.recruser.com/cv-list?ids=${possibleCvIds}`);
+        recruserProfileLink.setAttribute('target', '_blank');
     }
 }
 
-function SetupSelectVacancyStep() {
+async function parseCvAndSetCvId() {
+    window.recruserCvIsParsing = true;
+    window.recruserCvId = await parseAndSaveCv();
+    window.recruserCvIsParsing = false;
+}
+
+async function SetupSelectVacancyStep() {
     recruserSaveBlock.style.display = 'none';
     recruserSelectVacancyBlock.style.display = 'block';
+
+    window.recruserLastVacancy = await getFromLocalStorage('recruserLastVacancy');
+    if (window.recruserLastVacancy) {
+        recruserSelectVacancyAutocomplete.value = window.recruserLastVacancy.title;
+    }
 
     //https://leaverou.github.io/awesomplete/#advanced-examples
     let autocomplete = new Awesomplete(recruserSelectVacancyAutocomplete, {
@@ -103,30 +120,41 @@ function SetupSelectVacancyStep() {
         fetchVacancies(input).then(vacancies => {
             autocomplete.list = vacancies.map(v => v.title);
         });
+        selectVacancyValidationEl.style.display = 'none';
     };
-    recruserSelectVacancyNextStep.onclick = (e) => {
+    recruserSelectVacancyNextStep.onclick = async (e) => {
         e.preventDefault();
         let input = recruserSelectVacancyAutocomplete.value;
-        fetchVacancies(input).then(vacancies => {
-            if (vacancies.length == 1 && vacancies[0].title == input && vacancies[0].stepSystemId) {
-                window.recruserVacancy = {
-                    id: vacancies[0].id,
-                    title: vacancies[0].title,
-                    stepSystemId: vacancies[0].stepSystemId,
-                    relation: vacancies[0].recruiterRelation
-                };
+        if (!input.length) { // skip vacancy select
+            await setToLocalStorage('recruserLastVacancy', null);
+            window.recruserCandidateId = await saveCandidate(window.recruserCvId, null);
+            setStep(window.RecruserDoneStep);
+            return;
+        }
+
+        let vacancies = await fetchVacancies(input);
+        if (vacancies.length == 1 && vacancies[0].title == input && vacancies[0].stepSystemId) {
+            let targetVacancy = await fetchVacancyById(vacancies[0].id);
+
+            if (await doesCvAlreadyAttachedToVacancy(window.recruserCvId, targetVacancy.id)) {
+                selectVacancyValidationEl.style.display = 'block';
+                selectVacancyValidationEl.innerText = 'Already attached to this vacancy';
+            } else {
                 setStep(window.RecruserSelectStepStep);
+                await setToLocalStorage('recruserLastVacancy', targetVacancy);
+                window.recruserLastVacancy = targetVacancy;
+                window.recruserCandidateId = await saveCandidate(window.recruserCvId, window.recruserLastVacancy.id);
             }
-            else {
-                //show validation message that there are no such vacancy
-            }
-        });
+        }
+        else {
+            selectVacancyValidationEl.style.display = 'block';
+            selectVacancyValidationEl.innerText = 'No such vacancy';
+        }
     };
 }
 
-function setupSelectStepStep() {
-    console.log(window.recruserVacancy);
-    if (!window.recruserVacancy.stepSystemId) {
+async function setupSelectStepStep() {
+    if (!window.recruserLastVacancy.stepSystemId) {
         setStep(window.RecruserSelectStepStep);
     }
 
@@ -138,87 +166,150 @@ function setupSelectStepStep() {
         maxItems: 15,
         sort: (a, b) => a.order > b.order
     });
-    fetchSteps(window.recruserVacancy.stepSystemId, window.recruserVacancy.relation).then(stepsystem => {
+    fetchSteps(window.recruserLastVacancy.stepSystemId, window.recruserLastVacancy.recruiterRelation).then(stepsystem => {
         window.recruserStepSystem = stepsystem;
-        console.log('step-system', window.recruserStepSystem);
-
         autocomplete.list = stepsystem.steps.filter(s => s.canUse == true).map(s => s.title);
     });
-    recruserSelectStepNextStep.onclick = (e) => {
+
+    recruserSelectStepNextStep.oninput = () => {
+        selectStepValidationEl.style.display = 'none';
+    };
+    recruserSelectStepNextStep.onclick = async (e) => {
         e.preventDefault();
         let input = recruserSelectStepAutocomplete.value;
+        if (!input.length) { // skip step select
+            setStep(window.RecruserDoneStep);
+            //TODO: save to google sheet if spreadSheetUrl present
+            return;
+        }
+
         let possibleSteps = window.recruserStepSystem.steps.filter(s => s.title == input);
         if (possibleSteps.length == 1) {
             let targetStep = possibleSteps[0];
             window.recruserSelectedStepId = targetStep.id;
-            console.log(targetStep);
+
             setStep(window.RecruserDoneStep);
+            let comment = document.getElementById('recruser-step-comment').value;
+            await saveCandidateStep(window.recruserCandidateId, window.recruserSelectedStepId, comment)
+            //TODO: save to google sheet if spreadSheetUrl present
         }
         else {
-            //show validation message that there are no such vacancy
+            selectStepValidationEl.style.display = 'block';
+            selectStepValidationEl.innerHTML = 'No such step';
         }
     };
 }
 
 function setupDoneStep() {
+    recruserSelectVacancyBlock.style.display = 'none';
     recruserSelectStepBlock.style.display = 'none';
     recruserDoneBlock.style.display = 'block';
-
-    //TODO
-    //1. create endpoint that takes all info: CV, vacancyID, stepID
-    //or saveCV -> return ID - form and show Link
-    //meanwhile save candiate and add him to google sheet
-    //2. call asynchronously, don't waiting result
 }
 
-function parseCv() {
-    // return fetch(`${getApiHost()}/parser/cv`, {
-    //     method: 'POST',
-    //     headers: getHeaders(),
-    //     body: {
-    //         url: '',//TODO: get current url
-    //         html: document.body
-    //     }
-    // }).then(resp => {
-    //     let cv = resp.json();
-    //     //save CV and return cvID to window variable
-    // });
+async function getPossibleCvIdsByFullNameOrSoruceUrlInDb() {
+    return await fetch(`${getApiHost()}/parser/cv/existence-check`, {
+        method: 'POST',
+        headers: await getHeaders(),
+        body: JSON.stringify({
+            url: location.href,
+            html: document.body.innerHTML
+        })
+    }).then(resp => resp.json());
 }
 
-function fetchVacancies(text) {
+async function parseAndSaveCv() {
+    return await fetch(`${getApiHost()}/cv/from-page`, {
+        method: 'POST',
+        headers: await getHeaders(),
+        body: JSON.stringify({
+            url: location.href,
+            html: document.body.innerHTML
+        })
+    }).then(resp => resp.json());
+}
+
+async function saveCandidate(cvId, vacancyId) {
+    return await fetch(`${getApiHost()}/candidates`, {
+        method: 'POST',
+        headers: await getHeaders(),
+        body: JSON.stringify({
+            cvId: cvId,
+            vacancyId: vacancyId
+        })
+    }).then(resp => resp.json());
+}
+
+async function saveCandidateStep(candidateId, stepId, comment) {
+    return await fetch(`${getApiHost()}/candidates/${candidateId}/step-transactions`, {
+        method: 'POST',
+        headers: await getHeaders(),
+        body: JSON.stringify({
+            newStepId: stepId,
+            comment: comment
+        })
+    }).then(resp => resp.json());
+}
+
+async function fetchVacancies(text) {
     return fetch(`${getApiHost()}/vacancies?count=5&vacancyTitle=${text}`, {
         method: 'GET',
-        headers: getHeaders()
+        headers: await getHeaders()
     }).then(resp => resp.json());
 }
 
-function fetchSteps(stepSystemId, relation) {
+async function fetchSteps(stepSystemId, relation) {
     return fetch(`${getApiHost()}/step-systems/${stepSystemId}?relation=${relation}`, {
         method: 'GET',
-        headers: getHeaders()
+        headers: await getHeaders()
     }).then(resp => resp.json());
 }
+
+async function fetchVacancyById(id) {
+    return fetch(`${getApiHost()}/vacancies/${id}`, {
+        method: 'GET',
+        headers: await getHeaders()
+    }).then(resp => resp.json());
+}
+
+async function doesCvAlreadyAttachedToVacancy(cvId, vacancyId) {
+    return fetch(`${getApiHost()}/cvs/${cvId}/vacancy/${vacancyId}`, {
+        method: 'GET',
+        headers: await getHeaders()
+    }).then(resp => resp.json());
+}
+
 
 function getApiHost() {
     return 'http://localhost:57492';
 }
 
-function getHeaders() {
+async function getHeaders() {
     return new Headers({
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${getUserToken()}`
+        'Authorization': `Bearer ${await getUserToken()}`
     });
 }
-function getUserToken() {
-    const dataStr = localStorage.getItem('recruserUser');
-    if (!dataStr || dataStr === 'null' || dataStr === 'undefined')
-        return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJMT0NBTCBBVVRIT1JJVFkiOiJBdXRoU2VydmVyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6InJlYzFAZ21haWwuY29tIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiUmVjcnVpdGVyIiwibmJmIjoxNTU3NzQwMzE3LCJleHAiOjE1NTgzNDUxMTcsImlzcyI6IkF1dGhTZXJ2ZXIiLCJhdWQiOiJBYXJzZXJBcGkifQ.tJZp4GFKJpTu_gnTVcWz1z2BFsgZB1ZbDWD0kRmKV4s";
-    return JSON.parse(dataStr).token;
+async function getUserToken() {
+    let user = await getFromLocalStorage('recruserUser');
+    if (!user) return '';
+    return user.token;
+}
+
+async function setUserIfTestingEnvironment() {
+    if (isTestingEnvironment()) {
+        await setToLocalStorage('recruserUser', {
+            email: "rec1@gmail.com",
+            expireInMs: 1558556419249.2485,
+            id: "0eb595e6-26e8-4902-bf69-65866478b516",
+            name: "rec1@gmail.com",
+            role: 0,
+            token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJMT0NBTCBBVVRIT1JJVFkiOiJBdXRoU2VydmVyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6InJlYzFAZ21haWwuY29tIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiUmVjcnVpdGVyIiwibmJmIjoxNTU3OTUxNjE5LCJleHAiOjE1NTg1NTY0MTksImlzcyI6IkF1dGhTZXJ2ZXIiLCJhdWQiOiJBYXJzZXJBcGkifQ.Gy5BWheLHp4F-98NyDN9G4YDADQAT8HzjzR6O62-wpk"
+        });
+    }
 }
 
 async function isUrlSupported(url) {
-    if (url.startsWith('file:///')) return true;
-    if(url.startsWith('https://extension-components.recruser.com')) return true;
+    if (isTestingEnvironment()) return true;
 
     let urlParts = await getSupportedUrlParts();
     return urlParts.find(part => url.includes(part)) != null
--- shared.js
+++ shared.js
@@ -15,3 +15,24 @@ async function getOrCreateFromCacheAsync(key, callback, minutes) {
     }));
     return data;
 }
+
+async function getFromLocalStorage(key) {
+    if (isTestingEnvironment()) {
+        return await Promise.resolve(JSON.parse(localStorage.getItem(key)));
+    } else {
+        return await new Promise(resolve => chrome.storage.local.get(key, resolve));
+    }
+}
+async function setToLocalStorage(key, data) {
+    if (isTestingEnvironment()) {
+        Promise.resolve(localStorage.setItem(key, JSON.stringify(data)));
+    } else {
+        return new Promise(resolve => {
+            chrome.storage.local.set(data, resolve);
+        });
+    }
+}
+
+function isTestingEnvironment() {
+    return location.href.startsWith('file:///') || location.href.startsWith('https://extension-components');
+}
